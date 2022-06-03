91e3b8eedd621626743181c565f3f5a73756783c
Vinay0910 <vinayrshelke0910@gmail.com>
2022-06-03 19:48:02 +0530

    done

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '52e461b1-1f21-451e-a62a-03d8d5a1bd55';
+export const MODAL_CLOSE_SEC = 2.5;
